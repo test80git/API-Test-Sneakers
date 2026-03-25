8be14b091371ebbe7b0addf0dc72477a3e35ffa2
test80git <test80git@yandex.ru>
2026-03-25 19:32:25 +0300

    init commit

---
+++ src/test/java/ru/kuz/autotestforsneakerstore/AutoTestForSneakerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuz.autotestforsneakerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestForSneakerStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
